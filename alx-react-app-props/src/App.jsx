bc3fe70ae7028a41f26b39b4490bb88971610df7
Richmond AmedzeDoe <guturich@gmail.com>
2025-07-24 23:25:45 +0000

    Last commit

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,3 +1,9 @@
+import React from 'react';
+import WelcomeMessage from './components/WelcomeMessage';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
 import ProfilePage from './components/ProfilePage';
 import UserContext from './components/UserContext';
 import UserDetails from './components/UserDetails';
@@ -7,11 +13,21 @@ function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
   return (
+    <div>
+      <WelcomeMessage />
+            <Header />
+            <MainContent />
+            <Footer />
+            <UserProfile
+              name="Alice"
+              age="25"
+              bio="Loves hiking and photography" />
     <UserContext.Provider value={userData}>
       <ProfilePage />
       <UserDetails />
       <UserInfo />
     </UserContext.Provider>
+    </div>
   );
 }
 
