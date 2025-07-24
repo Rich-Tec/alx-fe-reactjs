5d3de2c4c579bd0ea0f3f926876727f5913d0445
Richmond AmedzeDoe <guturich@gmail.com>
2025-07-24 11:24:36 +0000

    Added a new folder

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent () {
+    return(
+        <main>
+             <p style={{ textAlign: 'left', color: 'slateblue' }}>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    );
+}
+
+export default MainContent;
