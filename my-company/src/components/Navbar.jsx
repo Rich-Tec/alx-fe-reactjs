fcf03d9853e25030d8117e90ef5bb6c216fd357b
Richmond AmedzeDoe <guturich@gmail.com>
2025-07-25 17:13:58 +0000

    Added style to Navbar

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,7 +2,14 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav style={{ padding: '10px', backgroundColor: '#eee', marginBottom: '20px' }}>
+    <nav
+      style={{
+        padding: '10px',
+        backgroundColor: '#eee',
+        display: 'flex',
+        justifyContent: 'center',
+      }}
+    >
       <Link to="/" style={{ margin: '0 15px' }}>Home</Link>
       <Link to="/about" style={{ margin: '0 15px' }}>About</Link>
       <Link to="/services" style={{ margin: '0 15px' }}>Services</Link>
