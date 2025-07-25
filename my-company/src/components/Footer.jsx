4d47be1573cc2abd00f3f0130c59f4fd425fdcd0
Richmond AmedzeDoe <guturich@gmail.com>
2025-07-25 17:03:25 +0000

    Created my-company website

---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer style={{ textAlign: 'center', padding: '10px', marginTop: '30px', backgroundColor: '#eee' }}>
+      © 2025 My Company. All rights reserved.
+    </footer>
+  );
+}
+
+export default Footer;
