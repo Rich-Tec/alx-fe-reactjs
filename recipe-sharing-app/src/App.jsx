c80cdc41b44550194b9ddfc96e677bbd9e683963
Richmond AmedzeDoe <guturich@gmail.com>
2025-08-01 14:34:31 +0000

    Made some update

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,10 +1,11 @@
 // src/App.jsx
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import AddRecipeForm from './components/AddRecipeForm';
 import DeleteRecipeButton from './components/DeleteRecipeButton';
 import EditRecipeForm from './components/EditRecipeForm';
 import FavoritesList from './components/FavoritesList';
-import Home from './components/Home';
+import HomePage from './components/Home'; // ✅ use the page component
 import RecipeDetails from './components/RecipeDetails';
 import RecipeList from './components/RecipeList';
 import RecommendationsList from './components/RecommendationsList';
@@ -14,17 +15,17 @@ const App = () => {
   return (
     <Router>
       <Routes>
-        <Route>
-      <AddRecipeForm />
-      <DeleteRecipeButton />
-      <EditRecipeForm />
-      <FavoritesList />
-      <omePage />
-      <RecipeDetails />
-      <RecipeList />
-      <RecommendationsList />
-      <SearchBar />
-        </Route>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/recipe/:id" element={<RecipeDetails />} />
+        <Route path="/add" element={<AddRecipeForm />} />
+        <Route path="/edit/:id" element={<EditRecipeForm />} />
+        <Route path="/edit/:id" element={<DeleteRecipeButton />} />
+        <Route path="/edit/:id" element={<FavoritesList />} />
+        <Route path="/edit/:id" element={<RecipeList />} />
+        <Route path="/edit/:id" element={<RecommendationsList />} />
+        <Route path="/edit/:id" element={<SearchBar />} />
+
+
       </Routes>
     </Router>
   );
