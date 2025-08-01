888b633a86121dcf2c6a0cd5924bce5bafe846e5
Richmond AmedzeDoe <guturich@gmail.com>
2025-08-01 14:01:53 +0000

    Updated recipeStore

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -5,28 +5,45 @@ export const useRecipeStore = create((set) => ({
   favorites: [],
   recommendations: [],
 
-  //Add setRecipes
-  setRecipes: (newRecipes) =>
-    set(() => ({
-      recipes: newRecipes,
-    })),
-
-  // Add addRecipe
+  //Add a new recipe
   addRecipe: (newRecipe) =>
     set((state) => ({
       recipes: [...state.recipes, newRecipe],
     })),
 
+  //Replace all recipes
+  setRecipes: (newRecipes) =>
+    set(() => ({
+      recipes: newRecipes,
+    })),
+
+  //Update a recipe by ID
+  updateRecipe: (updatedRecipe) =>
+    set((state) => ({
+      recipes: state.recipes.map((recipe) =>
+        recipe.id === updatedRecipe.id ? updatedRecipe : recipe
+      ),
+    })),
+
+  //Delete a recipe by ID
+  deleteRecipe: (recipeId) =>
+    set((state) => ({
+      recipes: state.recipes.filter((recipe) => recipe.id !== recipeId),
+    })),
+
+  //Add to favorites
   addFavorite: (recipeId) =>
     set((state) => ({
       favorites: [...new Set([...state.favorites, recipeId])],
     })),
 
+  //Remove from favorites
   removeFavorite: (recipeId) =>
     set((state) => ({
       favorites: state.favorites.filter((id) => id !== recipeId),
     })),
 
+  //Generate random recommendations from favorites
   generateRecommendations: () =>
     set((state) => {
       const recommended = state.recipes.filter(
